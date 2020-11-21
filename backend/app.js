0fc0d2105794ba136e8b79a28f1e86cbc4eac0cf
Laura Queyla <laura.queyla@gmail.com>
2020-11-21 21:10:41 +0100

    Mise en place de MongoDB

--- backend/app.js
+++ backend/app.js
@@ -7,3 +7,10 @@ app.use((req, res) => {
  });
 
 module.exports = app;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://lau:<sc4dXJbm9KgmB2V>@cluster0.mkvl6.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
