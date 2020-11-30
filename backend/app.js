9a894c0d143236427cc8047679135db533812fc2
Laura Queyla <laura.queyla@gmail.com>
2020-11-30 15:44:32 +0100

    Ajout des dossiers routes et controllers

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
- });
+const sauceRoutes = require('./routes/sauces')
 
-module.exports = app;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://lau:<sc4dXJbm9KgmB2V>@cluster0.mkvl6.mongodb.net/<dbname>?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://lau:sc4dXJbm9KgmB2V@cluster0.mkvl6.mongodb.net/<dbname>?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(bodyParser.json());
+
+
+app.use('/api/sauces', sauceRoutes);
+
+
+module.exports = app;
+
+
+
