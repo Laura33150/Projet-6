1bd718ac9a2e9e06821447d851fd3747957ba48d
Laura Queyla <laura.queyla@gmail.com>
2020-12-02 09:53:15 +0100

    Ajout de la variable d'environnement

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,8 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    //TODO voir comment supprimer cetee clé secrete "variable d'environnement"
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, `${process.env.RND_TKN}`);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
